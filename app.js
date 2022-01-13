ecc198af189a6e1d7eff2db359ccc61225fd4ff0
Ricky <support@nyrro.ch>
2022-01-13 10:55:44 +0100

    Install pug

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
-const port = 3000
+const port = 4000
 
-app.listen(port, () => console.log('listen on port 3000'))
+app.listen(port, () => console.log('listen on port 4000'))
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
